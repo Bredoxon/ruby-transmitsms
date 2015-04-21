5ddec64baa3705660648bb230098d165d29bedef
maiah <maiah@burstsms.com>
2015-04-21 13:46:31 +1000

    [#92307458] Added rspec for negative/failure testing

--- spec/sendsms_spec.rb
+++ spec/sendsms_spec.rb
@@ -12,7 +12,7 @@ describe "Sms methods" do
     setup_mock()
 
     # Call method to test
-    response = @sms.send("Testing API now", "61422222222")
+    response = @sms.send("Testing API to field", "61422222222")
 
     # Assert result
     expect(response.code).to(eq(200))
@@ -24,13 +24,23 @@ describe "Sms methods" do
     setup_mock_list_id()
 
     # Call method to test
-    response = @sms.send("Testing API now", nil, nil, nil, 55314)
+    response = @sms.send("Testing API with list_id field", nil, nil, nil, 55314)
 
     # Assert result
     expect(response.code).to(eq(200))
     expect(response.body["list"]["id"]).to(eq(55314))
     expect(response.body["error"]["code"]).to(eq("SUCCESS"))
     expect(response.body["error"]["description"]).to(eq("OK"))
+  end
+
+  it "should not send sms for missing 'to' field or list_id' field" do
+    setup_mock_error()
+
+    response = @sms.send("Test error sending")
+
+    expect(response.code).to(eq(400))
+    expect(response.body["error"]["code"]).to(eq("FIELD_INVALID"))
+    expect(response.body["error"]["description"]).to(eq("You must provide either 'list_id' or 'to'"))
   end
 
   def setup_mock()
@@ -57,6 +67,20 @@ describe "Sms methods" do
           "error" => { "code" => "SUCCESS", "description" => "OK"}
         },
         :code => 200
+      )))
+
+    @sms.sms_api = @mock_sms_api
+  end
+
+  def setup_mock_error()
+    @mock_sms_api = double("SmsApi")
+
+    allow(@mock_sms_api).to(receive(:send)
+      .and_return(OpenStruct.new(
+        :body => { 
+          "error" => { "code" => "FIELD_INVALID", "description" => "You must provide either 'list_id' or 'to'" }
+        },
+        :code => 400
       )))
 
     @sms.sms_api = @mock_sms_api
